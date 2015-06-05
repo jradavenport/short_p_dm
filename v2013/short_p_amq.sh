aab3c62285b32fe497e697c94878a09617cc9482
James Davenport <jradavenport@gmail.com>
2015-06-05 15:06:16 -0700

    add old stuff

---
+++ v2013/short_p_amq.sh
@@ -0,0 +1,24 @@
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=1572802;" > 1572802.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=4175707;" > 4175707.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=4953358;" > 4953358.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=5685704;" > 5685704.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=5791720;" > 5791720.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=6117832;" > 6117832.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=6715960;" > 6715960.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=6871390;" > 6871390.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=7547969;" > 7547969.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=7800087;" > 7800087.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=8012943;" > 8012943.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=8331561;" > 8331561.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=8416220;" > 8416220.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=9075708;" > 9075708.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=9205855;" > 9205855.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=9726699;" > 9726699.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=9761113;" > 9761113.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=9992083;" > 9992083.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=10515986;" >10515986.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=10548640;" >10548640.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=11124203;" >11124203.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=11395206;" >11395206.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=12258225;" >12258225.lc
+mysql -e "SELECT * FROM Kepler.source where LCFLAG=1 and KEPLERID=12365719;" >12365719.lc
